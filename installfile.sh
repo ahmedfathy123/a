66cdf66183fa5cc004863c2cf240cbea500aafa6
ahmedfathy123 <148116291+ahmedfathy123@users.noreply.github.com>
2025-07-24 19:36:53 +0300

    Update installfile.sh

--- installfile.sh
+++ installfile.sh
@@ -1,5 +1,5 @@
 rm -f cyberpanel.sh
 rm -f install.tar.gz
-curl --silent -o cyberpanel.sh "https://cyberpanel.sh/?dl&$SERVER_OS" 2>/dev/null
+curl --silent -o cyberpanel.sh "https://cyberpanel.sh/?dl&CentOS8" 2>/dev/null
 chmod +x cyberpanel.sh
 ./cyberpanel.sh $@
